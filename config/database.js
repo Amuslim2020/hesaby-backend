457e18045dda483e17b7003d1e74d31ca450708a
Amuslim2020 <abdullah.muslim2021@gmail.com>
2025-04-16 14:19:56 +0330

    users and accounts crud is done

--- config/database.js
+++ config/database.js
@@ -1,11 +1,14 @@
 const mysql = require('mysql2');
-require('dotenv').config();
+require('dotenv').config()
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,     // e.g., localhost or IP
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-});
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    
+})
+.promise();
 
-module.exports = pool.promise();
+
+module.exports = pool;
